d24f4c5072e69bd8c8ce7200aa7407d5b8222d7c
Frank Onojoserio <francot3230@gmail.com>
2021-07-27 11:47:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+app = express();
+const port = 8000;
+const userRoutes = require('./routers/userRoute')
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!')
+  })
+
+app.use('/api/user', userRoutes)
+
+
+
+
+
+
+
+
+
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://127.0.0.1:${port}`)
+  })
+ 
+  
+  
+
